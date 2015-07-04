bb8d5ba478ce1168f2a43655fa3b695bc8674209
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-04 12:48:41 -0400

    Tweak and move around things.

--- lib/utils.dart
+++ lib/utils.dart
@@ -14,27 +14,17 @@ Stdin get _stdin => stdin;
 class BetterProcessResult extends ProcessResult {
   final String output;
 
-  BetterProcessResult(int pid, int exitCode, stdout, stderr, this.output) :
-  super(pid, exitCode, stdout, stderr);
+  BetterProcessResult(int pid, int exitCode, stdout, stderr, this.output)
+      : super(pid, exitCode, stdout, stderr);
 }
 
-Future<BetterProcessResult> exec(
-    String executable,
-    {
-    List<String> args: const [],
-    String workingDirectory,
-    Map<String, String> environment,
-    bool includeParentEnvironment: true,
-    bool runInShell: false,
-    stdin,
-    ProcessHandler handler,
-    OutputHandler stdoutHandler,
-    OutputHandler stderrHandler,
-    OutputHandler outputHandler,
-    File outputFile,
-    bool inherit: false,
-    bool writeToBuffer: false
-    }) async {
+Future<BetterProcessResult> exec(String executable,
+    {List<String> args: const [], String workingDirectory,
+    Map<String, String> environment, bool includeParentEnvironment: true,
+    bool runInShell: false, stdin, ProcessHandler handler,
+    OutputHandler stdoutHandler, OutputHandler stderrHandler,
+    OutputHandler outputHandler, File outputFile, bool inherit: false,
+    bool writeToBuffer: false}) async {
   IOSink raf;
 
   if (outputFile != null) {
@@ -46,17 +36,15 @@ Future<BetterProcessResult> exec(
   }
 
   try {
-    Process process = await Process.start(
-        executable,
-        args,
+    Process process = await Process.start(executable, args,
         workingDirectory: workingDirectory,
         environment: environment,
         includeParentEnvironment: includeParentEnvironment,
-        runInShell: runInShell
-    );
+        runInShell: runInShell);
 
     if (raf != null) {
-      await raf.writeln("[${currentTimestamp}] == Executing ${executable} with arguments ${args} (pid: ${process.pid}) ==");
+      await raf.writeln(
+          "[${currentTimestamp}] == Executing ${executable} with arguments ${args} (pid: ${process.pid}) ==");
     }
 
     var buff = new StringBuffer();
@@ -130,18 +118,14 @@ Future<BetterProcessResult> exec(
     var pid = process.pid;
 
     if (raf != null) {
-      await raf.writeln("[${currentTimestamp}] == Exited with status ${code} ==");
+      await raf
+          .writeln("[${currentTimestamp}] == Exited with status ${code} ==");
       await raf.flush();
       await raf.close();
     }
 
     return new BetterProcessResult(
-        pid,
-        code,
-        ob.toString(),
-        eb.toString(),
-        buff.toString()
-    );
+        pid, code, ob.toString(), eb.toString(), buff.toString());
   } finally {
     if (raf != null) {
       await raf.flush();
@@ -151,7 +135,8 @@ Future<BetterProcessResult> exec(
 }
 
 Future<String> findExecutable(String name) async {
-  var paths = Platform.environment["PATH"].split(Platform.isWindows ? ";" : ":");
+  var paths =
+      Platform.environment["PATH"].split(Platform.isWindows ? ";" : ":");
   var tryFiles = [name];
 
   if (Platform.isWindows) {
@@ -193,4 +178,311 @@ Future<bool> isPortOpen(int port, {String host: "0.0.0.0"}) async {
 
 String get currentTimestamp {
   return new DateTime.now().toString();
+}
+
+String fseType(FileSystemEntity entity) {
+  if (entity is Directory) {
+    return "directory";
+  } else if (entity is File) {
+    return "file";
+  } else if (entity is Link) {
+    return "link";
+  }
+
+  return "unknown";
+}
+
+Future<bool> setWifiNetwork(
+    String interface, String ssid, String password) async {
+  if (Platform.isMacOS) {
+    var args = ["-setairportnetwork", interface, ssid];
+
+    if (password != null && password.isNotEmpty) {
+      args.add(password);
+    }
+
+    var result = await Process.run("networksetup", args);
+
+    if (result.exitCode != 0) {
+      return false;
+    }
+
+    return true;
+  } else {
+    var args = [interface, "essid", ssid];
+
+    if (password != null && password.isNotEmpty) {
+      args.addAll(["key", password]);
+    }
+
+    var result = await Process.run("iwconfig", args);
+
+    if (result.exitCode != 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+Future startHotspot() async {
+  await Process.run("hotspotd", ["start"]);
+}
+
+Future stopHotspot() async {
+  await Process.run("hotspotd", ["stop"]);
+  await Process.run("pkill", ["hostapd"]);
+}
+
+Future<bool> isHotspotOn() async {
+  return (await Process.run("pgrep", ["hostapd"])).exitCode == 0;
+}
+
+Future<bool> isWifiInterface(String name) async {
+  if (Platform.isMacOS) {
+    var result = await Process.run("networksetup", ["-listallhardwareports"]);
+
+    if (result.exitCode != 0) {
+      return false;
+    }
+
+    List<String> lines =
+        result.stdout.split("\n").map((x) => x.trim()).toList();
+
+    lines.removeWhere((x) => x.isEmpty);
+
+    var buff = [];
+
+    for (var line in lines) {
+      if (line.startsWith("Device: ${name}")) {
+        var hp = buff[buff.length - 1];
+        return hp.contains("Wi-Fi");
+      } else {
+        buff.add(line);
+      }
+    }
+
+    return false;
+  } else {
+    var result = await Process.run("iwconfig", [name]);
+
+    if (result.exitCode != 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+Future configureNetworkAutomatic(String interface) async {
+  if (Platform.isMacOS) {
+    var service = await getNetworkServiceForInterface(interface);
+    var args = ["-setdhcp", service];
+
+    return Process.run("networksetup", args).then((result) {
+      return result.exitCode == 0;
+    });
+  }
+
+  var resultA =
+      await Process.run("ifconfig", [interface, "0.0.0.0", "0.0.0.0"]);
+  if (resultA.exitCode != 0) {
+    return false;
+  }
+
+  await Process.run("dhclient", ["-r", interface]);
+  var resultB = await Process.run("dhclient", [interface]);
+
+  return resultB.exitCode == 0;
+}
+
+Future configureNetworkManual(
+    String interface, String ip, String netmask, String gateway) async {
+  if (Platform.isWindows) {
+    throw new Exception("Unsupported on Windows");
+  }
+
+  if (Platform.isMacOS) {
+    var service = await getNetworkServiceForInterface(interface);
+    var args = ["-setmanual", service, ip, netmask, gateway];
+
+    return Process.run("networksetup", args).then((result) {
+      return result.exitCode == 0;
+    });
+  }
+
+  await Process.run("killall", ["dhclient"]);
+
+  var resultA =
+      await Process.run("route", ["add", "default", "gw", gateway, interface]);
+  if (resultA.exitCode != 0) {
+    return false;
+  }
+
+  var resultB =
+      await Process.run("ifconfig", [interface, ip, "netmask", netmask]);
+
+  return resultB.exitCode == 0;
+}
+
+Future<String> serializeNetworkState() async {
+  var x = [];
+  var inter = await NetworkInterface.list();
+  for (NetworkInterface i in inter) {
+    x.add({
+      "name": i.name,
+      "addresses": i.addresses.map((it) => it.address).toList()
+    });
+  }
+  return JSON.encode(x);
+}
+
+Future<String> getWifiNetwork(String interface) async {
+  if (Platform.isMacOS) {
+    var result =
+        await Process.run("networksetup", ["-getairportnetwork", interface]);
+    if (result.exitCode != 0) {
+      return null;
+    } else {
+      return result.stdout.trim().replaceAll("Current Wi-Fi Network: ", "");
+    }
+  } else {
+    var result = await Process.run("iwconfig", [interface]);
+    if (result.exitCode != 0) {
+      return null;
+    } else {
+      try {
+        var line = result.stdout.split("\n").first;
+        var regex = new RegExp(r'ESSID\:"(.*)"');
+        return regex.firstMatch(line).group(1);
+      } catch (e) {
+        return null;
+      }
+    }
+  }
+}
+
+Future<String> getNetworkServiceForInterface(String interface) async {
+  var result = await Process.run("networksetup", ["-listnetworkserviceorder"]);
+  List<String> lines = result.stdout.split("\n");
+  var results = {};
+  lines.where((x) => x.startsWith("(Hardware Port:")).forEach((x) {
+    var mn = x.substring(1, x.length - 1).split(",");
+    results[mn[1].split(":")[1].trim()] = mn[0].split(":")[1].trim();
+  });
+  return results[interface];
+}
+
+Future<List<WifiNetwork>> scanWifiNetworks(String interface) async {
+  if (Platform.isMacOS) {
+    var airport =
+        "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport";
+    var result = await Process.run(airport, [interface, "scan"]);
+    if (result.exitCode != 0) {
+      return [];
+    }
+    String out = result.stdout;
+    List<String> lines = out.split("\n");
+    var firstLine = lines.removeAt(0);
+    var ssidEnd = firstLine.indexOf("SSID") + 4;
+
+    var networks = [];
+
+    for (var line in lines) {
+      if (line.trim().isEmpty) {
+        continue;
+      }
+
+      var ssid = line.substring(0, ssidEnd).trim();
+      var hasSecurity = !line.contains("NONE");
+      var network = new WifiNetwork(ssid, hasSecurity);
+      networks.add(network);
+    }
+
+    return networks;
+  } else {
+    var result = await Process.run("iwlist", [interface, "scan"]);
+
+    if (result.exitCode != 0) {
+      return [];
+    }
+
+    List<String> lines = result.stdout.split("\n");
+    lines.removeAt(0);
+
+    var buff = [];
+    var networks = [];
+    for (var line in lines) {
+      if (line.trim().isEmpty) {
+        continue;
+      }
+
+      if (buff.isEmpty && line.trim().startsWith("Cell ")) {
+        continue;
+      }
+
+      if (line.trim().startsWith("Cell ") || line == lines.last) {
+        var content = buff.join("\n");
+        var regex = new RegExp(r'ESSID\:"(.*)"');
+        var ssid = regex.firstMatch(content).group(1);
+        var hasSecurity = content.contains("Encryption key:on");
+        var network = new WifiNetwork(ssid, hasSecurity);
+        networks.add(network);
+        buff.clear();
+        continue;
+      }
+
+      buff.add(line);
+    }
+
+    return networks;
+  }
+}
+
+class WifiNetwork {
+  final String ssid;
+  final bool hasSecurity;
+
+  WifiNetwork(this.ssid, this.hasSecurity);
+
+  List toRows() => [ssid, hasSecurity];
+}
+
+class System {
+  static void reboot() {
+    var result = Process.runSync("reboot", []);
+
+    if (result.exitCode != 0) {
+      print("ERROR: Failed to reboot.");
+    }
+  }
+
+  static void shutdown() {
+    var result = Process.runSync(
+        isUnix ? "poweroff" : "shutdown", isUnix ? [] : ["-h", "now"]);
+
+    if (result.exitCode != 0) {
+      print("ERROR: Failed to shutdown. Exit Code: ${result.exitCode}");
+    }
+  }
+}
+
+bool get isUnix => Platform.isLinux || Platform.isMacOS;
+
+Future<List<String>> getCurrentNameServers() async {
+  var file = new File("/etc/resolv.conf");
+
+  if (!(await file.exists())) {
+    return [];
+  }
+
+  var lines = (await file.readAsLines())
+      .map((x) => x.trim())
+      .where((x) => x.isNotEmpty && !x.startsWith("#"))
+      .toList();
+
+  return lines
+      .where((x) => x.startsWith("nameserver "))
+      .map((x) => x.replaceAll("nameserver ", ""))
+      .toList();
 }
